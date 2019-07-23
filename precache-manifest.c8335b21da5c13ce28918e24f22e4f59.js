1b6f243985a5873ac9b96b1557e7c7140d109051
Michael Clemons <mwclemons@gmail.com>
2019-07-23 16:23:27 -0500

    Updates

---
+++ precache-manifest.c8335b21da5c13ce28918e24f22e4f59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447219b975ce7cbae0682dd0d7fb33f6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b2c2196b0be539a2a629",
+    "url": "/clicky-game/static/css/main.94485ce0.chunk.css"
+  },
+  {
+    "revision": "bc9e213220f85ecf4864",
+    "url": "/clicky-game/static/js/2.501d70a8.chunk.js"
+  },
+  {
+    "revision": "b2c2196b0be539a2a629",
+    "url": "/clicky-game/static/js/main.8cb3dfa3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "4eaccf1ba62fed8544cde90de2502ef9",
+    "url": "/clicky-game/static/media/dirk.4eaccf1b.jpg"
+  },
+  {
+    "revision": "22a7d7f2b06190a677a7ee11be1dbecd",
+    "url": "/clicky-game/static/media/giannis.22a7d7f2.jpg"
+  },
+  {
+    "revision": "2daca3a2f41c893b5f161867b4f17d8e",
+    "url": "/clicky-game/static/media/kemp.2daca3a2.jpg"
+  },
+  {
+    "revision": "0f61692db2b25bb215343b85da453f9c",
+    "url": "/clicky-game/static/media/kobe.0f61692d.jpg"
+  },
+  {
+    "revision": "07b56122607c5c085a2a934a01d349fb",
+    "url": "/clicky-game/static/media/lebron.07b56122.jpg"
+  }
+]);
